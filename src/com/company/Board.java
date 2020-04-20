05bfb57b1e887f1539eb68824742a75f7a61ef65
brdayauon <brdayauon@gmail,com>
2020-04-20 15:22:10 -0700

    Fixed the printing of the board

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -3,63 +3,45 @@ import java.util.*;
 import java.util.ArrayList;
 
 public class Board {
+    ArrayList<Board> successors;
+
     private String[][] board;
     private int boardLength = 8;
     private String[][] gameState;
+    private String xPosition;
+    private String oPosition;
 
-    ArrayList<Board> children;
-
-    public Board()
-    {
+    public Board() {
+        successors = new ArrayList<Board>();
         gameState = new String[boardLength][boardLength];
         gameState[0][0] = "X";
-        gameState[7][7] = "Y";
-
+        gameState[7][7] = "O";
+        xPosition = "A1";
+        oPosition = "H8";
     }
 
-
-    public Board (String[][] gameState) //put positionX and positionO
-    {
+    public Board (String[][] gameState, String xPosition, String oPosition) {
         this.gameState = gameState;
+        successors = new ArrayList<Board>();
     }
-
-
-//    public Board(){ //prints the Board
-//            // public static void printMatrix(char[][] board) {
-//            // 8x8 board
-//            char[][] boardState = new char[boardLength][boardLength];
-//            for (int i = 0; i < boardLength; i++) {
-//                for (int j = 0; j < boardLength; j++) {
-//                    boardState[i][j] = '-';
-//                }
-//                System.out.println();
-//            }
-////            System.out.println("------------------------------");
-//
-//            this.board = boardState;
-//        }
 
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();
-        //res.append("------------------------------\n");
+
         for (int i = 0; i < boardLength; i++) {
             for (int j = 0; j < boardLength; j++) {
                 if (gameState[i][j] == null){
-                    res.append('-');
-                }
-                res.append(board[i][j] +  ' ');
-                // System.out.print(board[i][j] + "|");
+                    res.append("-");
+                }else  {res.append(gameState[i][j]);}
+
+                res.append(" ");
             }
             res.append("\n");
         }
-     //  res.append("------------------------------\n");
 
         return res.toString();
     }
-
-
-
 
 
     //// Print the current board state
