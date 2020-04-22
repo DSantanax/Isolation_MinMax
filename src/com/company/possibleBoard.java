a40a082923eede09c638b1863f7e8b3037c3de5f
brdayauon <brdayauon@gmail,com>
2020-04-21 19:18:45 -0700

    implemented copyMatrix to generate children

--- src/com/company/possibleBoard.java
+++ src/com/company/possibleBoard.java
@@ -1,23 +1,60 @@
 package com.company;
 
-public class possibleBoard {
-    private char[][] board;
+import java.util.ArrayList;
+
+public class possibleBoard extends Board{
+    private String[][] board;
     int boardLength = 8;
 
-    public possibleBoard(int boardLength)
-    {
-        this.boardLength = boardLength;
-        board = new char[boardLength][boardLength];
 
-        for(int i = 0; i < boardLength; i++)
-        {
-            for(int j = 0; j < boardLength; j++)
-            {
-                this.board[i][j] = '-';
-            }
+    public String[][] copyMatrix(Board board)
+    {
+        String[][] gameState = board.getArr(); //gameState points to Board
+
+        //edgeCase if gameState is ever null
+        if (gameState == null){
+            return null;
         }
+
+        //create another 2d array that will copy elements of Board
+        String res[][]  = new String[boardLength][boardLength];
+        for(int i = 0; i < boardLength; i+= 1){
+            res[i] = gameState[i].clone();
+        }
+
+        return res;
     }
 
-    //do alpha beta pruning here? *might make another class* 
+//    //takes in a possible board and produces other possible boards O
+//    public ArrayList<possibleBoard> generateSuccessors(Board board)
+//    {
+//
+////        Board childBoard[boardLength][boardLength] = board;
+////
+////        for(int i = 0; i < boardLength; i++)
+////        {
+////            for(int j = 0; j < boardLength; j++)
+////            {
+////
+////            }
+////        }
+//
+//
+//
+//    }
+
+    public static void main(String[] args){
+        Board board = new Board();
+        possibleBoard pb = new possibleBoard();
+
+        System.out.println(board.toString());
+        System.out.println("hi");
+
+        String[][] res = pb.copyMatrix(board);
+        System.out.println(res.toString());
+
+    }
+
+    //do alpha beta pruning here? *might make another class*
 
 }
