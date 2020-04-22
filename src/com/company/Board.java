b887f38895902cc89f4d0a12aa481a9b4db67765
brdayauon <brdayauon@gmail,com>
2020-04-21 20:08:54 -0700

    updated toString() to fit to assignment standards

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -7,35 +7,70 @@ public class Board {
 
     private String[][] board;
     private int boardLength = 8;
+    private String player;
     private String[][] gameState;
     private String xPosition;
     private String oPosition;
 
     public Board() {
-        successors = new ArrayList<Board>();
+        successors = new ArrayList<>();
         gameState = new String[boardLength][boardLength];
         gameState[0][0] = "X";
         gameState[7][7] = "O";
         xPosition = "A1";
         oPosition = "H8";
+
     }
 
-    public Board (String[][] gameState, String xPosition, String oPosition) {
+    public String[][] getArr(){
+        return gameState;
+    }
+
+    public Board(String[][] gameState)
+    {
         this.gameState = gameState;
+    }
+
+    public Board (String[][] gameState, String player, String xPosition, String oPosition) {
+        this.gameState = gameState;
+        this.player = player;
+        this.xPosition = xPosition;
+        this.oPosition = oPosition;
         successors = new ArrayList<Board>();
     }
 
     @Override
-    public String toString() {
+    public String toString(){
         StringBuilder res = new StringBuilder();
+        String letter = "";
+        res.append("  1 2 3 4 5 6 7 8" + "\n");
 
-        for (int i = 0; i < boardLength; i++) {
-            for (int j = 0; j < boardLength; j++) {
-                if (gameState[i][j] == null){
-                    res.append("-");
-                }else  {res.append(gameState[i][j]);}
+        for(int i = 0; i < boardLength; i+= 1){
 
-                res.append(" ");
+            if (i == 0)
+                letter = "A";
+            else if(i == 1)
+                letter = "B";
+            else if(i == 2)
+                letter = "C";
+            else if(i == 3)
+                letter = "D";
+            else if(i == 4)
+                letter = "E";
+            else if(i == 5)
+                letter = "F";
+            else if(i == 6)
+                letter =  "G";
+            else if(i == 7)
+                letter = "H";
+
+            res.append(letter + " ");
+            for(int j = 0; j < boardLength; j+= 1){
+                if(gameState[i][j] == null)
+                    res.append("-" + " ");
+                else {
+                    res.append(gameState[i][j] + " ");
+                }
             }
             res.append("\n");
         }
@@ -44,9 +79,8 @@ public class Board {
     }
 
 
+
     //// Print the current board state
     //// Get all legal moves
 
 }
-
-
